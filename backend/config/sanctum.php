8428728e68d6e23fe580c237583dd735b7ec69f6
pomidornijfrukt <jekabs.kuzmenko@gmail.com>
2025-06-03 19:48:56 +0300

    backend sanctum auth

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,localhost:5500,localhost:8888,127.0.0.1,127.0.0.1:8000,127.0.0.1:5500,127.0.0.1:8888,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
